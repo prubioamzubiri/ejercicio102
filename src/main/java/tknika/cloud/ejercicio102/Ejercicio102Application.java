de62b3c295244c214ec6a08a03e2b9227597aba5
Pablo Rubio <prubioam@zubirimanteo.com>
2022-09-29 11:06:10 +0200

    Initial commit

---
+++ src/main/java/tknika/cloud/ejercicio102/Ejercicio102Application.java
@@ -0,0 +1,13 @@
+package tknika.cloud.ejercicio102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio102Application.class, args);
+	}
+
+}
---
+++ src/test/java/tknika/cloud/ejercicio102/Ejercicio102ApplicationTests.java
@@ -0,0 +1,13 @@
+package tknika.cloud.ejercicio102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
